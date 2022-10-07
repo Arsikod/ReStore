b408a0f404cf7ff7bbe495cf0fa07550d7d73f12
Nurbol Nygmanov <nurbol.nygmanov@mediamonks.com>
2022-10-07 07:18:04 +0600

    end of section 2

---
+++ API/Data/Migrations/20221006165151_TableUpdate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class TableUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityStock",
+                table: "Products",
+                newName: "QuantityInStock");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "QuantityInStock",
+                table: "Products",
+                newName: "QuantityStock");
+        }
+    }
+}
